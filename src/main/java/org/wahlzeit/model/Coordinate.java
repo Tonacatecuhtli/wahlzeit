b611a6646f665b406a8d3b153dcee6ccae113870
Maximilian Wisgickl <wisgicklma@gmail.com>
2018-10-25 13:09:35 +0200

    implemented Coordination class plus tests

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -11,12 +11,30 @@ public class Coordinate {
     }
 
     protected double getDistance (Coordinate other) {
+        double xDif = Math.pow((other.x - this.x), 2);
+        double yDif = Math.pow((other.y - this.y), 2);
+        double zDif = Math.pow((other.z - this.z), 2);
 
-        return 1;
+        return Math.sqrt(xDif + yDif + zDif);
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        // check if it's a assignable
+        if (!Coordinate.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+
+        final Coordinate other = (Coordinate) obj;
+        return this.isEqual(other);
+    }
+
+
     protected boolean isEqual (Coordinate other) {
-        // check variables for nill
-        return true;
+        return this.x == other.x && this.y == other.y && this.z == other.z;
     }
 }
--- src/test/java/org/wahlzeit/model/CoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CoordinateTest.java
@@ -2,14 +2,19 @@ package org.wahlzeit.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.*;
 
 public class CoordinateTest {
 
+    private static final Logger log = Logger.getLogger(CoordinateTest.class.getName());
+
     private Coordinate c1 = new Coordinate(1,1,1);
     private Coordinate c2 = new Coordinate(2,2,2);
+
+    // delta of which assertEquals double, test variables can be different
+    private static final double epsilon = 1e-10;
 
     @Test
     public void testIsEqual() {
@@ -17,5 +22,11 @@ public class CoordinateTest {
         assertFalse(c1.isEqual(c2));
     }
 
+    @Test
+    public void testDistance() {
+        // c2 with 2,2,2 - c1 with 1,1,1 = 1.7320508075688772;
+        assertEquals(c1.getDistance(c2),1.7320508075688772, epsilon);
+        assertEquals(c1.getDistance(c1), 0, epsilon);
+    }
 
 }
