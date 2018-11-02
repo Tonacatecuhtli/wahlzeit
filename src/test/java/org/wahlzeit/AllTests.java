d78194290deed85cd473d846f8673520e5573d0c
Maximilian Wisgickl <wisgicklma@gmail.com>
2018-11-02 19:11:50 +0100

    Added AllTests.java and changed the entry point in build.gradle
    This leads to three tests of the AbstractAdapterTest failing

---
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2006-2009 by Dirk Riehle, http://dirkriehle.com
+ *
+ * This file is part of the Wahlzeit photo rating application.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+package org.wahlzeit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import org.wahlzeit.handlers.TellFriendTest;
+import org.wahlzeit.model.*;
+import org.wahlzeit.model.persistence.AbstractAdapterTest;
+import org.wahlzeit.services.EmailServiceTestSuite;
+import org.wahlzeit.services.LogBuilderTest;
+import org.wahlzeit.utils.StringUtilTest;
+import org.wahlzeit.utils.VersionTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        // model
+        TellFriendTest.class,
+        AbstractAdapterTest.class,
+        AccessRightsTest.class,
+        CoordinateTest.class,
+        FlagReasonTest.class,
+        GenderTest.class,
+        GuestTest.class,
+        LocationTest.class,
+        PhotoFilterTest.class,
+        TagsTest.class,
+        UserStatusTest.class,
+        ValueTest.class,
+        // services
+        EmailServiceTestSuite.class,
+        LogBuilderTest.class,
+        // utils
+        StringUtilTest.class,
+        VersionTest.class,
+})
+
+public class AllTests {
+}
