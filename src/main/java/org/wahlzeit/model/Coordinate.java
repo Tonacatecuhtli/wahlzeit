12d701dc2ad89d0e53fea9d0a9bbfdc082058362
Maximilian Wisgickl <wisgicklma@gmail.com>
2018-11-03 15:54:05 +0100

    added spacing comments to Coordinate.java

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -21,14 +21,23 @@
 package org.wahlzeit.model;
 
 public class Coordinate {
+    /**
+     *
+     */
     private double x, y, z;
 
+    /**
+     *
+     */
     public Coordinate (double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
+    /**
+     *
+     */
     protected double getDistance (Coordinate other) {
         double xDif = Math.pow((other.x - this.x), 2);
         double yDif = Math.pow((other.y - this.y), 2);
@@ -37,6 +46,9 @@ public class Coordinate {
         return Math.sqrt(xDif + yDif + zDif);
     }
 
+    /**
+     *
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -52,7 +64,9 @@ public class Coordinate {
         return this.isEqual(other);
     }
 
-
+    /**
+     *
+     */
     protected boolean isEqual (Coordinate other) {
         return this.x == other.x && this.y == other.y && this.z == other.z;
     }
