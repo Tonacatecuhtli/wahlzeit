d2455631766eb60017c2de6cab6160d3ab82eea0
Maximilian Wisgickl <wisgicklma@gmail.com>
2018-11-03 15:54:33 +0100

    added spacing comments to Location

--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -21,13 +21,21 @@
 package org.wahlzeit.model;
 
 public class Location {
-
+    /**
+     *
+     */
     protected Coordinate coordinate = null;
 
+    /**
+     *
+     */
     public Coordinate getCoordinate() {
         return coordinate;
     }
 
+    /**
+     *
+     */
     public void setCoordinate(Coordinate coordinate) {
         this.coordinate = coordinate;
     }
