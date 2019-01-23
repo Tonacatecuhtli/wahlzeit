04cddb3459f1de80fff8f2ebb4f1482760c4f389
Maximilian Wisgickl <wisgicklma@gmail.com>
2019-01-23 11:37:17 +0100

    NebulaPhoto has a Nebula

--- src/main/java/org/wahlzeit/model/NebulaPhoto.java
+++ src/main/java/org/wahlzeit/model/NebulaPhoto.java
@@ -13,12 +13,18 @@ import java.util.logging.Logger;
 )
 public class NebulaPhoto extends Photo {
 
-    private static final Logger log = Logger.getLogger(NebulaPhoto.class.getName());
+    /**
+     * @methodType getter
+     * @return
+     */
+    public Nebula getNebula() {
+        return nebula;
+    }
 
-    private double magnitude;
+    private Nebula nebula;
 
     /**
-     *
+     * @methodtype constructor
      */
     public NebulaPhoto() {
         // log.info("Default NebulaPhoto constructor");
@@ -38,37 +44,7 @@ public class NebulaPhoto extends Photo {
     /**
      * @methodtype constructor
      */
-    public NebulaPhoto(PhotoId myId, double magnitude) {
-        // log.info("Id/ magnitude NebulaPhoto constructor");
-        id = myId;
-        magnitude = magnitude;
-        incWriteCount();
-    }
-
-    @Override
-    public String toString() {
-        return "NebulaPhoto{" +
-                "magnitude=" + magnitude +
-                ", id=" + id +
-                ", ownerId='" + ownerId + '\'' +
-                ", images=" + images +
-                ", ownerNotifyAboutPraise=" + ownerNotifyAboutPraise +
-                ", ownerEmailAddress=" + ownerEmailAddress +
-                ", ownerLanguage=" + ownerLanguage +
-                ", width=" + width +
-                ", height=" + height +
-                ", maxPhotoSize=" + maxPhotoSize +
-                ", tags=" + tags +
-                ", status=" + status +
-                ", praiseSum=" + praiseSum +
-                ", noVotes=" + noVotes +
-                ", noVotesAtLastNotification=" + noVotesAtLastNotification +
-                ", creationTime=" + creationTime +
-                ", ending='" + ending + '\'' +
-                ", location=" + location +
-                ", idLong=" + idLong +
-                ", parent=" + parent +
-                ", writeCount=" + writeCount +
-                '}';
+    public NebulaPhoto(String typeName, int discoveryTimestamp, Location location){
+        nebula = NebulaManager.getInstance().createNebula(typeName, discoveryTimestamp, location);
     }
 }
